92ebe74ae9feaa462bb34345a3dae452772b021e
oboyle3 <oboyle3patrick@gmail.com>
2026-02-02 20:29:54 -0500

    Add MLB Team model and landing page

--- baseballcrm/league/models.py
+++ baseballcrm/league/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 # lets make a Player model
 
+class Team(models.Model):
+    name = models.CharField(max_length=25)
+    city = models.CharField(max_length=25)
+    def __str__(self):
+        return f"{self.city} self.name"
--- baseballcrm/league/views.py
+++ baseballcrm/league/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Team
 # Create your views here.
+def landing(request):
+    teams = Team.objects.all()
+    return render(request, "league/landing.html", {"teams": teams})
