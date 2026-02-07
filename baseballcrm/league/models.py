6ea8f8bd6ced3bc2adbf1e74cc2699b5516ffe2e
oboyle3 <oboyle3patrick@gmail.com>
2026-02-06 23:03:47 -0500

    added players model

--- baseballcrm/league/models.py
+++ baseballcrm/league/models.py
@@ -7,3 +7,25 @@ class Team(models.Model):
     city = models.CharField(max_length=25)
     def __str__(self):
         return f"{self.city} self.name"
+    
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    POSITIONS = [
+        ("P", "Pitcher"),
+        ("C", "Catcher"),
+        ("1B", "First Base"),
+        ("2B", "Second Base"),
+        ("3B", "Third Base"),
+        ("SS", "Shortstop"),
+        ("LF", "Left Field"),
+        ("CF", "Center Field"),
+        ("RF", "Right Field"),
+        ("DH", "Designated Hitter"),
+    ]
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    position = models.CharField(max_length=2,
+                                choices=POSITIONS)
+    def __str__(self):
+        return f"{self.name}"
--- baseballcrm/league/views.py
+++ baseballcrm/league/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from .models import Team
+from .models import Team, Player
 # Create your views here.
 def landing(request):
     teams = Team.objects.all()
-    return render(request, "league/landing.html", {"teams": teams})
+    players = Player.objects.all()
+    print(players)
+    context = {
+        "teams": teams,
+        "players":players,
+    }
+    return render(request, "league/landing.html",context )
+
